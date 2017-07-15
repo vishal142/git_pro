78b9a7e22fd4e1e6bf0d010756758abdb823d148
Vishal Gupta <vishal.k.gupta@massoftind.com>
2017-07-15 17:01:04 +0530

    config and htaccess update

--- apis/application/config/config.php
+++ apis/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://'. $_SERVER['SERVER_NAME'].'/Demo_ci/apis/';
+$config['base_url'] = 'http://'. $_SERVER['SERVER_NAME'].'/git_pro/apis/';
 
 /*
 |--------------------------------------------------------------------------
